854a85b7cc5a0fc048e7cb20a21ac343281b1539
Iqbal Athorid <iqbalathoridd@gmail.com>
2021-01-18 18:21:18 +0800

    created myorder screens

---
+++ src/components/OrderCard.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {Card, CardItem, Left, Right, Body} from 'native-base';
+
+export default function OrderCard() {
+  return (
+    <View style={styles.cardWrapper}>
+      <Card style={styles.card}>
+        <CardItem style={styles.headerCard}>
+          <Left>
+            <Text style={styles.orderNo}>Order №1947034</Text>
+          </Left>
+          <Right>
+            <Text style={styles.date}>05-12-2019</Text>
+          </Right>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <View style={styles.infoWrapper}>
+              <View style={styles.trackingWrapper}>
+                <Text style={styles.orderLabel}>Tracking number:</Text>
+              </View>
+              <View>
+                <Text style={styles.orderValue}>IW3475453455</Text>
+              </View>
+            </View>
+            <View style={styles.infoWrapper}>
+              <View style={styles.trackingWrapper}>
+                <Text style={styles.orderLabel}>Quantity:</Text>
+              </View>
+              <View>
+                <Text style={styles.orderValue}>3</Text>
+              </View>
+            </View>
+            <View style={styles.infoWrapper}>
+              <View style={styles.trackingWrapper}>
+                <Text style={styles.orderLabel}>Total Amount:</Text>
+              </View>
+              <View>
+                <Text style={styles.orderPrice}>112$</Text>
+              </View>
+            </View>
+            <View style={styles.statusWrapper}>
+              <View style={styles.statusDisplay}>
+                <Text style={styles.statusValue}>Delivered</Text>
+              </View>
+            </View>
+          </Body>
+        </CardItem>
+      </Card>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  cardWrapper: {
+    marginBottom: 20,
+    paddingLeft: 2,
+  },
+  infoWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 7,
+  },
+  trackingWrapper: {
+    marginRight: 10,
+  },
+  orderLabel: {
+    fontSize: 14,
+    lineHeight: 20,
+    color: '#9B9B9B',
+  },
+  orderNo: {
+    fontSize: 16,
+    lineHeight: 16,
+    fontWeight: 'bold',
+    color: '#222222',
+  },
+  orderValue: {
+    fontSize: 14,
+    lineHeight: 20,
+    color: '#222222',
+  },
+  orderPrice: {
+    fontSize: 14,
+    lineHeight: 20,
+    fontWeight: 'bold',
+    color: '#222222',
+  },
+  statusWrapper: {
+    width: '100%',
+  },
+  statusDisplay: {
+    alignSelf: 'flex-end',
+  },
+  statusValue: {
+    fontSize: 14,
+    lineHeight: 20,
+    color: '#2AA952',
+  },
+  card: {
+    paddingBottom: 13,
+    borderRadius: 8,
+    elevation: 2,
+  },
+  headerCard: {
+    borderRadius: 8,
+    paddingBottom: 0,
+  },
+  date: {
+    fontSize: 14,
+    lineHeight: 20,
+    color: '#9B9B9B',
+  },
+});
--- src/screns/MyProfile/MyOrders.js
+++ src/screns/MyProfile/MyOrders.js
@@ -1,10 +1,44 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, SafeAreaView, FlatList} from 'react-native';
+import OrderCard from '../../components/OrderCard';
+
+const data = [
+  {id: '1', name: 'Hallo'},
+  {id: '2', name: 'Hallo'},
+];
 
 export default function MyOrders() {
   return (
-    <View>
-      <Text>MyOrders </Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <View style={styles.labelWrapper}>
+          <Text style={styles.labelText}>MyOrders </Text>
+        </View>
+        <FlatList
+          data={data}
+          renderItem={({item}) => <OrderCard data={item} />}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 30,
+  },
+  content: {
+    paddingLeft: 14,
+    paddingRight: 15,
+  },
+  labelText: {
+    fontSize: 34,
+    lineHeight: 34,
+    color: '#222222',
+    fontWeight: 'bold',
+  },
+  labelWrapper: {
+    marginBottom: 24,
+  },
+});

67716474b4d6825174bf91c533ad9d34d5809b4e
Iqbal Athorid <iqbalathoridd@gmail.com>
2021-01-19 23:11:28 +0800

    added validation to change adress

--- src/helpers/formValidation.js
+++ src/helpers/formValidation.js
@@ -11,3 +11,21 @@ export const changePassword = Yup.object().shape({
     .oneOf([Yup.ref('newPassword'), null], 'Password must match')
     .required('Repeat password is Required'),
 });
+
+export const adressSchema = Yup.object().shape({
+  saveAs: Yup.string().trim().required('Save as is required'),
+  recipient: Yup.string().min(4, 'To sort').required('Recipient is required'),
+  adress: Yup.string()
+    .trim()
+    .min(20, 'Adress to sort')
+    .required('Adress is required'),
+  city: Yup.string().min(4, 'City to sirt').required('City is required'),
+  postalCode: Yup.string()
+    .min(5, 'Invalid code')
+    .max(5, 'Invalid code')
+    .required('Postal Code is required'),
+  phoneNumber: Yup.string()
+    .min(12, 'Invalid number')
+    .max(13, 'Invalid number')
+    .required('Phone number is required'),
+});
--- src/screns/ChangeShippingAdress.js
+++ src/screns/ChangeShippingAdress.js
@@ -1,87 +1,205 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import {Item, Input, Label, Button} from 'native-base';
+import {Formik} from 'formik';
+
+import {adressSchema} from '../helpers/formValidation';
 
 export default function ChangeShippingAdress() {
   return (
     <View style={styles.content}>
-      <View>
-        <View style={styles.InputWrapper}>
-          <View style={styles.inputDisplay}>
-            <Item style={styles.item} stackedLabel>
-              <Label style={styles.labelInput}>
-                Save address as (ex : home address, office address)
-              </Label>
-              <Input
-                placeholder="Input home"
-                placeholderTextColor="#9b9b9b"
-                style={styles.input}
-              />
-            </Item>
+      <Formik
+        initialValues={{
+          saveAs: '',
+          recipient: '',
+          adress: '',
+          city: '',
+          postalCode: '',
+          phoneNumber: '',
+        }}
+        validationSchema={adressSchema}
+        onSubmit={(values) => console.log(values)}>
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          errors,
+          touched,
+        }) => (
+          <View>
+            <View style={styles.InputWrapper}>
+              <View style={styles.inputDisplay}>
+                <Item
+                  style={
+                    errors.saveAs && touched.saveAs
+                      ? styles.itemError
+                      : styles.item
+                  }
+                  stackedLabel>
+                  <Label style={styles.labelInput}>
+                    Save address as (ex : home address, office address)
+                  </Label>
+                  <Input
+                    placeholder="Input home"
+                    placeholderTextColor="#9b9b9b"
+                    style={styles.input}
+                    name="saveAs"
+                    value={values.saveAs}
+                    onChangeText={handleChange('saveAs')}
+                    onBlur={handleBlur('saveAs')}
+                  />
+                </Item>
+                <View style={styles.errorWrapper}>
+                  {errors.saveAs && touched.saveAs && (
+                    <Text style={styles.textError}>{errors.saveAs}</Text>
+                  )}
+                </View>
+              </View>
+              <View style={styles.inputDisplay}>
+                <Item
+                  style={
+                    errors.recipient && touched.recipient
+                      ? styles.itemError
+                      : styles.item
+                  }
+                  stackedLabel>
+                  <Label style={styles.labelInput}>Recipient’s name</Label>
+                  <Input
+                    placeholder="Input recepient's"
+                    placeholderTextColor="#9b9b9b"
+                    style={styles.input}
+                    name="recipient"
+                    value={values.recipient}
+                    onChangeText={handleChange('recipient')}
+                    onBlur={handleBlur('recipient')}
+                  />
+                </Item>
+                <View style={styles.errorWrapper}>
+                  {errors.recipient && touched.recipient && (
+                    <Text style={styles.textError}>{errors.recipient}</Text>
+                  )}
+                </View>
+              </View>
+            </View>
+            <View style={styles.InputWrapper}>
+              <View style={styles.inputDisplay}>
+                <Item
+                  style={
+                    errors.adress && touched.adress
+                      ? styles.itemError
+                      : styles.item
+                  }
+                  stackedLabel>
+                  <Label style={styles.labelInput}>Address</Label>
+                  <Input
+                    placeholder="Input adress"
+                    placeholderTextColor="#9b9b9b"
+                    style={styles.input}
+                    name="adress"
+                    value={values.adress}
+                    onChangeText={handleChange('adress')}
+                    onBlur={handleBlur('adress')}
+                  />
+                </Item>
+                <View style={styles.errorWrapper}>
+                  {errors.adress && touched.adress && (
+                    <Text style={styles.textError}>{errors.adress}</Text>
+                  )}
+                </View>
+              </View>
+              <View style={styles.inputDisplay}>
+                <Item
+                  style={
+                    errors.city && touched.city ? styles.itemError : styles.item
+                  }
+                  stackedLabel>
+                  <Label style={styles.labelInput}>City or Subdistrict</Label>
+                  <Input
+                    placeholder="Input City or Subdistrict"
+                    placeholderTextColor="#9b9b9b"
+                    style={styles.input}
+                    name="city"
+                    value={values.city}
+                    onChangeText={handleChange('city')}
+                    onBlur={handleBlur('city')}
+                  />
+                </Item>
+                <View style={styles.errorWrapper}>
+                  {errors.city && touched.city && (
+                    <Text style={styles.textError}>{errors.city}</Text>
+                  )}
+                </View>
+              </View>
+              <View style={styles.inputDisplay}>
+                <Item
+                  style={
+                    errors.postalCode && touched.postalCode
+                      ? styles.itemError
+                      : styles.item
+                  }
+                  stackedLabel>
+                  <Label style={styles.labelInput}>Postal code</Label>
+                  <Input
+                    placeholder="Input code"
+                    placeholderTextColor="#9b9b9b"
+                    style={styles.input}
+                    name="postalCode"
+                    keyboardType="number-pad"
+                    value={values.postalCode}
+                    onChangeText={handleChange('postalCode')}
+                    onBlur={handleBlur('postalCode')}
+                  />
+                </Item>
+                <View style={styles.errorWrapper}>
+                  {errors.postalCode && touched.postalCode && (
+                    <Text style={styles.textError}>{errors.postalCode}</Text>
+                  )}
+                </View>
+              </View>
+            </View>
+            <View style={styles.InputWrapper}>
+              <View style={styles.inputDisplay}>
+                <Item
+                  style={
+                    errors.phoneNumber && touched.phoneNumber
+                      ? styles.itemError
+                      : styles.item
+                  }
+                  stackedLabel>
+                  <Label style={styles.labelInput}>
+                    recipient's telephone number
+                  </Label>
+                  <Input
+                    placeholder="Input number"
+                    placeholderTextColor="#9b9b9b"
+                    style={styles.input}
+                    name="phoneNumber"
+                    keyboardType="phone-pad"
+                    value={values.phoneNumber}
+                    onChangeText={handleChange('phoneNumber')}
+                    onBlur={handleBlur('phoneNumber')}
+                  />
+                </Item>
+                <View style={styles.errorWrapper}>
+                  {errors.phoneNumber && touched.phoneNumber && (
+                    <Text style={styles.textError}>{errors.phoneNumber}</Text>
+                  )}
+                </View>
+              </View>
+            </View>
+            <View>
+              <Button
+                onPress={handleSubmit}
+                style={styles.btnSave}
+                full
+                rounded>
+                <Text style={styles.btnText}>SAVE ADDRESS</Text>
+              </Button>
+            </View>
           </View>
-          <View style={styles.inputDisplay}>
-            <Item style={styles.item} stackedLabel>
-              <Label style={styles.labelInput}>Recipient’s name</Label>
-              <Input
-                placeholder="Input recepient's"
-                placeholderTextColor="#9b9b9b"
-                style={styles.input}
-              />
-            </Item>
-          </View>
-        </View>
-        <View style={styles.InputWrapper}>
-          <View style={styles.inputDisplay}>
-            <Item style={styles.item} stackedLabel>
-              <Label style={styles.labelInput}>Address</Label>
-              <Input
-                placeholder="Input adress"
-                placeholderTextColor="#9b9b9b"
-                style={styles.input}
-              />
-            </Item>
-          </View>
-          <View style={styles.inputDisplay}>
-            <Item style={styles.item} stackedLabel>
-              <Label style={styles.labelInput}>City or Subdistrict</Label>
-              <Input
-                placeholder="Input City or Subdistrict"
-                placeholderTextColor="#9b9b9b"
-                style={styles.input}
-              />
-            </Item>
-          </View>
-          <View style={styles.inputDisplay}>
-            <Item style={styles.item} stackedLabel>
-              <Label style={styles.labelInput}>Postal code</Label>
-              <Input
-                placeholder="Input code"
-                placeholderTextColor="#9b9b9b"
-                style={styles.input}
-              />
-            </Item>
-          </View>
-        </View>
-        <View style={styles.InputWrapper}>
-          <View style={styles.inputDisplay}>
-            <Item style={styles.item} stackedLabel>
-              <Label style={styles.labelInput}>
-                recipient's telephone number
-              </Label>
-              <Input
-                placeholder="Input number"
-                placeholderTextColor="#9b9b9b"
-                style={styles.input}
-              />
-            </Item>
-          </View>
-        </View>
-        <View>
-          <Button style={styles.btnSave} full rounded>
-            <Text style={styles.btnText}>SAVE ADDRESS</Text>
-          </Button>
-        </View>
-      </View>
+        )}
+      </Formik>
     </View>
   );
 }
@@ -124,6 +242,10 @@ const styles = StyleSheet.create({
     borderBottomColor: '#000000',
     borderBottomWidth: 1.3,
   },
+  itemError: {
+    borderBottomColor: 'red',
+    borderBottomWidth: 1.3,
+  },
   btnSave: {
     backgroundColor: '#DB3022',
   },
@@ -132,5 +254,13 @@ const styles = StyleSheet.create({
     lineHeight: 20,
     fontWeight: '500',
     color: '#FFFFFF',
+  },
+  textError: {
+    fontSize: 10,
+    marginLeft: 5,
+    color: 'red',
+  },
+  errorWrapper: {
+    height: 3,
   },
 });
